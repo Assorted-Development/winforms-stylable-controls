5e87ccf6961c2dfadb3bab1c2b4fa0df4e74de90
wolframhaussig@aol.com <wolframhaussig@aol.com>
2023-04-03 18:33:41 +0200

    support for more usecases

---
+++ Controls/StylableComboBox.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MFBot_1701_E.CustomControls
+{
+    /// <summary>
+    /// the default combobox does not allow styling the background - only the background of the dropdown
+    /// </summary>
+    public class StylableComboBox : ComboBox
+    {
+        public StylableComboBox()
+        {
+            this.DrawMode = DrawMode.OwnerDrawFixed;
+            this.DrawItem += StylableComboBox_DrawItem;
+        }
+
+        private void StylableComboBox_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            string value = "";
+            if(e.Index >= 0)
+            {
+                var val = Items[e.Index];
+                var t = val.GetType();
+                var valProp = t.GetProperty("Value");
+                if(valProp != null)
+                {
+                    value = valProp.GetValue(val).ToString();
+                }
+                else
+                {
+                    value = Items[e.Index].ToString();
+                }
+            }
+            SolidBrush bgBrush = new SolidBrush(BackColor);
+            var brush = new SolidBrush(ForeColor);
+            e.Graphics.FillRectangle(bgBrush, this.ClientRectangle);
+            e.Graphics.DrawString(value, e.Font, brush, e.Bounds, StringFormat.GenericDefault);
+            e.DrawFocusRectangle();
+        }
+    }
+}
