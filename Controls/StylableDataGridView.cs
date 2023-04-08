dd864ffb83d67264457df0096558b281a124a512
Robin Freund <info@nockiro.de>
2023-04-08 15:39:29 +0200

    More datagridview adjustments + scrollbar via native windows 10+ api

--- Controls/StylableDataGridView.cs
+++ Controls/StylableDataGridView.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace MFBot_1701_E.CustomControls
 {
     public class StylableDataGridView : DataGridView
     {
-        private FlatScrollBar _vScrollBar;
-        private FlatScrollBar _hScrollBar;
         private Form _parentForm;
 
         public new bool DoubleBuffered
@@ -27,146 +26,7 @@ namespace MFBot_1701_E.CustomControls
         {
             DoubleBuffered = true;
         }
-
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            base.OnPaint(e);
-
-            if (_vScrollBar is not null)
-            {
-                if (IsScrollbarVisible(true))
-                {
-                    int columnHeadersHeight = (ColumnHeadersHeight / Rows[0].Height);
-                    int rowTotalCount = RowCount - columnHeadersHeight;
-
-                    _vScrollBar.Minimum = 0;
-                    _vScrollBar.Maximum = rowTotalCount - 1 - columnHeadersHeight;
-                    _vScrollBar.LargeChange = 5;
-                    _vScrollBar.SmallChange = 1;
-
-                    AdjustVScrollBarSize();
-                    AdjustVScrollBarLocation();
-
-                    _vScrollBar.Visible = true;
-                }
-                else
-                {
-                    _vScrollBar.Visible = false;
-                }
-            }
-
-            if (_hScrollBar is not null)
-            {
-                if (IsScrollbarVisible(false))
-                {
-                    int frozenColumnsWidth = Columns.GetColumnsWidth(DataGridViewElementStates.Frozen);
-                    int columnTotalWidth =
-                        Columns.GetColumnsWidth(DataGridViewElementStates.Visible);
-
-                    AdjustHScrollBarSize();
-                    AdjustHScrollBarLocation();
-
-                    _hScrollBar.Minimum = 0;
-                    _hScrollBar.Maximum = columnTotalWidth - _hScrollBar.Width;
-                    _hScrollBar.LargeChange = Math.Max(2, _hScrollBar.Maximum / 2);
-                    _hScrollBar.SmallChange = 1;
-
-                    _hScrollBar.Visible = true;
-                }
-                else
-                {
-                    _hScrollBar.Visible = false;
-                }
-            }
-        }
-
-        protected override void OnHandleCreated(EventArgs e)
-        {
-            base.OnHandleCreated(e);
-
-            const int minimum = 0;
-            _vScrollBar = new FlatScrollBar() { Orientation = ScrollOrientation.VerticalScroll, Value = minimum, Minimum = minimum };
-            ParentForm.Controls.Add(_vScrollBar);
-            _vScrollBar.BringToFront();
-            _vScrollBar.Visible = false;
-
-            _vScrollBar.Scroll += this.VerticalScrollBar_Scroll;
-
-            _hScrollBar = new FlatScrollBar() { Orientation = ScrollOrientation.HorizontalScroll, Height = SystemInformation.HorizontalScrollBarHeight };
-            ParentForm.Controls.Add(_hScrollBar);
-            _hScrollBar.BringToFront();
-            _hScrollBar.Visible = false;
-
-            _hScrollBar.Scroll += this.HorizontalScrollBar_Scroll;
-        }
-
-        protected override void OnHandleDestroyed(EventArgs e)
-        {
-            base.OnHandleDestroyed(e);
-
-            if (_vScrollBar is not null)
-            {
-                if (ParentForm?.Controls.Contains(_vScrollBar) == true)
-                {
-                    ParentForm.Controls.Remove(_vScrollBar);
-                }
-                _vScrollBar.Dispose();
-            }
-
-            if (_hScrollBar is not null)
-            {
-                if (ParentForm?.Controls.Contains(_hScrollBar) == true)
-                {
-                    ParentForm.Controls.Remove(_hScrollBar);
-                }
-
-                _hScrollBar.Dispose();
-            }
-        }
-
-        protected override void OnSizeChanged(EventArgs e)
-        {
-            base.OnSizeChanged(e);
-
-            AdjustVScrollBarSize();
-            AdjustVScrollBarLocation();
-
-            AdjustHScrollBarSize();
-            AdjustHScrollBarLocation();
-        }
-
-        protected override void OnLocationChanged(EventArgs e)
-        {
-            base.OnLocationChanged(e);
-
-            AdjustVScrollBarLocation();
-            AdjustHScrollBarLocation();
-        }
-
-        protected override void OnScroll(ScrollEventArgs se)
-        {
-            base.OnScroll(se);
-
-            if (se.ScrollOrientation == ScrollOrientation.VerticalScroll)
-            {
-                SetVScrollBarValue(se.NewValue);
-            }
-            else if (_hScrollBar is not null)
-            {
-                SetHScrollBarValue(se.NewValue);
-            }
-        }
-
-        protected override void OnVisibleChanged(EventArgs e)
-        {
-            base.OnVisibleChanged(e);
-
-            if (_vScrollBar is not null)
-                _vScrollBar.Visible = Visible;
-            if (_hScrollBar is not null)
-                _hScrollBar.Visible = Visible;
-        }
-
+        
         protected override void OnCellPainting(DataGridViewCellPaintingEventArgs args)
         {
             base.OnCellPainting(args);
@@ -207,123 +67,6 @@ namespace MFBot_1701_E.CustomControls
                 args.FormattingApplied = true;
             }
         }
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                try
-                {
-                    if (_vScrollBar is not null)
-                    {
-                        ParentForm.Controls.Remove(_vScrollBar);
-                        _vScrollBar.Dispose();
-                    }
-
-                    if (_hScrollBar is not null)
-                    {
-                        ParentForm.Controls.Remove(_hScrollBar);
-                        _hScrollBar.Dispose();
-                    }
-                }
-
-                catch (Exception)
-                {
-                    // Do nothing
-                }
-            }
-
-            base.Dispose(disposing);
-        }
-
-        private void VerticalScrollBar_Scroll(object sender, ScrollEventArgs e)
-        {
-            SetVScrollBarValue(e.NewValue);
-        }
-
-        private void HorizontalScrollBar_Scroll(object sender, ScrollEventArgs e)
-        {
-            SetHScrollBarValue(e.NewValue);
-        }
-
-        private void AdjustVScrollBarLocation()
-        {
-            if (_vScrollBar is not null)
-            {
-                Point positionOnForm = GetLocationOnForm(this);
-                _vScrollBar.Location = BorderStyle == BorderStyle.None
-                    ? new Point(positionOnForm.X + Width - SystemInformation.VerticalScrollBarWidth, positionOnForm.Y)
-                    : new Point(positionOnForm.X + Width - (SystemInformation.VerticalScrollBarWidth + 1), positionOnForm.Y + 1);
-            }
-        }
-
-        private void AdjustVScrollBarSize()
-        {
-            if (_vScrollBar is not null)
-            {
-                int h = Height;
-
-                if (BorderStyle != BorderStyle.None)
-                    h -= 2;
-                if (IsScrollbarVisible(false))
-                    h -= SystemInformation.HorizontalScrollBarHeight;
-
-                _vScrollBar.Height = h;
-            }
-        }
-
-        private void SetVScrollBarValue(int value)
-        {
-            if (_vScrollBar is not null)
-            {
-                _vScrollBar.Value = value;
-                FirstDisplayedScrollingRowIndex = Math.Min(value, Rows.Count - 1);
-            }
-        }
-
-        private void AdjustHScrollBarLocation()
-        {
-            Point positionOnForm = GetLocationOnForm(this);
-            if (_hScrollBar is not null)
-            {
-                _hScrollBar.Location = BorderStyle == BorderStyle.None
-                    ? new Point(positionOnForm.X, positionOnForm.Y + Height - SystemInformation.HorizontalScrollBarHeight)
-                    : new Point(positionOnForm.X + 1, positionOnForm.Y + Height - (SystemInformation.HorizontalScrollBarHeight + 1));
-            }
-        }
-
-        private void AdjustHScrollBarSize()
-        {
-            if (_hScrollBar is not null)
-            {
-                int w = Width;
-
-                if (BorderStyle != BorderStyle.None)
-                    w -= 2;
-                if (IsScrollbarVisible(true))
-                    w -= SystemInformation.VerticalScrollBarWidth;
-
-                _hScrollBar.Width = w;
-            }
-        }
-
-        private void SetHScrollBarValue(int value)
-        {
-            if (_hScrollBar is not null)
-            {
-                _hScrollBar.Value = value;
-                HorizontalScrollingOffset = value;
-            }
-        }
-
-        private bool IsScrollbarVisible(bool vertical)
-        {
-            if (vertical)
-            {
-                return VerticalScrollBar?.Visible ?? false;
-            }
-
-            return HorizontalScrollBar?.Visible ?? false;
-        }
 
         private bool IsRepeatedCellValue(int rowIndex, int colIndex)
         {
@@ -345,18 +88,6 @@ namespace MFBot_1701_E.CustomControls
             }
 
             return false;
-        }
-
-        private Point GetLocationOnForm(Control control)
-        {
-            // Skip this if the controls handle isn't yet created as this creates buggy behaviour
-            if (!control.IsHandleCreated)
-            {
-                return Point.Empty;
-            }
-
-            // Get position relative to screen and from that the position relative to the form
-            return ParentForm.PointToClient(control.PointToScreen(Point.Empty));
         }
     }
 }
